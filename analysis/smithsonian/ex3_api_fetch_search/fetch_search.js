92de1dbeeb1919aab300d64176a5a43a39340d45
chanelkim <kimc889@newschool.edu>
2024-03-21 16:00:59 -0400

    smithsonian image url from edan

--- analysis/smithsonian/ex3_api_fetch_search/fetch_search.js
+++ analysis/smithsonian/ex3_api_fetch_search/fetch_search.js
@@ -60,44 +60,28 @@ const dates = [
   "1890s",
   "1900s",
 ];
+const date = "1900s";
 
 // Constructing the search query
 // const search = `Flowers AND unit_code:"CHNDM" AND object_type:"Embroidery (visual works)" AND online_media_type:"Images"`;
-const search = `object_type:"Clay" `;
-
 // https://collections.si.edu/search/results.htm?q=Flowers&view=grid&fq=data_source%3A%22Cooper+Hewitt%2C+Smithsonian+Design+Museum%22&fq=online_media_type%3A%22Images%22&media.CC0=true&fq=object_type:%22Embroidery+%28visual+works%29%22
 
-// search: fetches an array of terms based on term category
-function fetchSearchData(searchTerm, date) {
-  const mediaType = `AND online_media_type:"Images" `;
-  const timeRange = `AND date:${date}`;
-  let url =
-    searchBaseURL +
-    "?api_key=" +
-    apiKey +
-    "&q=" +
-    searchTerm +
-    mediaType +
-    timeRange;
-  console.log(url);
-  window
-    .fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-for (const date of dates) {
-  fetchSearchData(search, date);
-}
+/* ---- SEARCH CODE EDIT ----
+// // const search = `object_type:"Decorative arts" AND place:"USA"`;
+// const search = `object_type:"Clay" `;
 
 // // search: fetches an array of terms based on term category
-// function fetchSearchData(searchTerm) {
-//   let url = searchBaseURL + "?api_key=" + apiKey + "&q=" + searchTerm;
+// function fetchSearchData(searchTerm, date) {
+//   const mediaType = `AND online_media_type:"Images" `;
+//   const timeRange = `AND date:${date}`;
+//   let url =
+//     searchBaseURL +
+//     "?api_key=" +
+//     apiKey +
+//     "&q=" +
+//     searchTerm +
+//     mediaType +
+//     timeRange;
 //   console.log(url);
 //   window
 //     .fetch(url)
@@ -110,8 +94,76 @@ for (const date of dates) {
 //     });
 // }
 
-// fetchSearchData(search);
-// console.log(search);
+// for (const date of dates) {
+//   fetchSearchData(search, date);
+// }
+// ------------
+*/
+
+// // ---- JESSIE'S CODE EDIT (results for image url) -----
+// source: https://github.com/JessieJessJe/major-studio-1/blob/master/qualitative_data/download_data.html
+// const url = `https://api.si.edu/openaccess/api/v1.0/search?q=online_visual_material:true+AND+type:edanmdm+AND+helen%20hyde+&api_key=cR993Jr3eLTyxizxqZ6zaLCjGq0JAKe7s84Cbcjw&rows=125`;
+// const url = `https://api.si.edu/openaccess/api/v1.0/search?q=online_visual_material:true+AND+type:edanmdm+AND=object_type:"Clay"+AND=online_media_type:"Images"+&api_key=cR993Jr3eLTyxizxqZ6zaLCjGq0JAKe7s84Cbcjw&rows=12`;
+
+const search = `object_type:"Clay" `;
+
+const mediaType = `AND online_media_type:"Images" `;
+const timeRange = `AND date:${date}`;
+
+let url =
+  searchBaseURL + "?api_key=" + apiKey + "&q=" + search + mediaType + timeRange;
+console.log(url);
+let data;
+let mylist = [];
+
+fetch(url)
+  .then((resp) => {
+    if (!resp.ok) {
+      throw new Error(`HTTP error ${resp.status}`);
+    }
+    return resp.json();
+  })
+  .then((d) => {
+    const data = d.response.rows;
+    const mylist = [];
+
+    data.forEach((e) => {
+      console.log(e);
+
+      const topics = e.content.indexedStructured.topic || "";
+
+      const mydata = {
+        id: e.id,
+        title: e.title,
+        image_link:
+          e.content.descriptiveNonRepeating.online_media?.media?.[0]?.thumbnail,
+        width:
+          e.content.descriptiveNonRepeating.online_media?.media?.[0]
+            ?.resources?.[0]?.width,
+        height:
+          e.content.descriptiveNonRepeating.online_media?.media?.[0]
+            ?.resources?.[0]?.height,
+        record_link: e.content.descriptiveNonRepeating.record_link,
+        medium: e.content.freetext.physicalDescription?.[0]?.content,
+        topic: topics,
+        date: e.content.freetext.date?.[0]?.content,
+      };
+
+      mylist.push(mydata);
+    });
+
+    // Process mylist further here
+    document.getElementById("totalResult").innerText = JSON.stringify(
+      mylist,
+      null,
+      "\t"
+    );
+  })
+  .catch((error) => {
+    console.error("Error fetching or processing data:", error);
+    // Handle the error appropriately, e.g., display an error message to the user
+  });
+// ------------
 
 // CARDS
 //---------------APP FUNCTION---------------
